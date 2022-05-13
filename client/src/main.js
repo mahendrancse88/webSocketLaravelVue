4781b1b8c1c272873343393ed98f6522f5f21854
Mahendran <test16@yopmail.com>
2022-05-13 19:27:12 +0800

    files added

---
+++ client/src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Echo from 'laravel-echo';
+
+ window.Pusher = require('pusher-js');
+
+ window.Echo = new Echo({
+     broadcaster: 'pusher',
+     key: process.env.VUE_APP_WEBSOCKETS_KEY,
+     wsHost: process.env.VUE_APP_WEBSOCKETS_SERVER,
+     wsPort:6001,
+     forceTLS: false,
+     disableStats: true
+ });
+
+createApp(App).mount('#app')
